c21d3d624b8a4cc2a604d0ed53a8e0e6cc922329
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-11-08 19:11:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using System.Threading.Tasks;
+using myStringer;
+
+
+namespace DemoAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("implementing Assemblies");
+
+            stringer myStringInstance = new stringer();
+            Console.WriteLine("Client code executes");
+            myStringInstance.stringermethod();
+        }
+    }
+}
---
+++ stringer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DemoAssemblies
+{
+    class stringer
+    {
+        public void stringermethod()
+        {
+            Console.WriteLine("implementing this method is defined in demoassemblies ");
+            
+
+
+        }
+
+    }
+}
